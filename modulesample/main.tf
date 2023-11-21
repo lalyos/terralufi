be54227085d20a6789fd941c7e248ff24fa404a1
lalyos <lalyos@yahoo.com>
2023-11-21 21:52:53 +0100

    format file

--- modulesample/main.tf
+++ modulesample/main.tf
@@ -1,15 +1,15 @@
 module "webserver" {
-  source  = "github.com/lalyos/terralufi.git//azure"
-    unumber = "u9999999"
-    location = "westeurope"
-    body = <<-EOF
-      <iframe src="https://giphy.com/embed/xk5jrZNNQjNF6fxRig" width="480" height="388" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/film-magyar-rgi-xk5jrZNNQjNF6fxRig">via GIPHY</a></p>
-    EOF
-    title = "The End"
-    color = "mediumpurple"
-    owner = "jeno@example.com"
-    costcenter = "99999"
-    packages = ["nginx", "git"]
+  source     = "github.com/lalyos/terralufi.git//azure"
+  unumber    = "u9999999"
+  location   = "westeurope"
+  body       = <<-EOF
+    <iframe src="https://giphy.com/embed/xk5jrZNNQjNF6fxRig" width="480" height="388" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/film-magyar-rgi-xk5jrZNNQjNF6fxRig">via GIPHY</a></p>
+  EOF
+  title      = "The End"
+  color      = "mediumpurple"
+  owner      = "jeno@example.com"
+  costcenter = "99999"
+  packages   = ["nginx", "git"]
 }
 
 output "publicip" {
