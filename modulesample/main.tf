84334a9372f1de33f3349d621dacaf25d2810784
lalyos <lalyos@yahoo.com>
2023-11-21 21:46:30 +0100

    use heredoc for multiline string

--- modulesample/main.tf
+++ modulesample/main.tf
@@ -2,9 +2,11 @@ module "webserver" {
   source = "github.com/lalyos/azure-web-module"
     unumber = "u9999999"
     location = "westeurope"
-    body = "Hello from Terraform"
-    title = "Welcome to Terraform"
-    color = "lightblue"
+    body = <<-EOF
+      <iframe src="https://giphy.com/embed/xk5jrZNNQjNF6fxRig" width="480" height="388" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/film-magyar-rgi-xk5jrZNNQjNF6fxRig">via GIPHY</a></p>
+    EOF
+    title = "The End"
+    color = "mediumpurple"
     owner = "jeno@example.com"
     costcenter = "99999"
     packages = ["nginx", "git"]
